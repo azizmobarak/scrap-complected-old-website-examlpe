97e58290f35f906a54e10c79ac637da6ea7a0b22
aziz mobarak <mobarakaziz9@gmail.com>
2021-05-16 23:00:24 +0100

    update

--- scrap.js
+++ scrap.js
@@ -1,13 +1,20 @@
 'use strict';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const puppeteer  = require('puppeteer-extra');
 const puppeteer_stealth = require('puppeteer-extra-plugin-stealth');
 const puppeteer_agent = require('puppeteer-extra-plugin-anonymize-ua');
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch')
+var request = require('request');
 
 puppeteer.use(puppeteer_agent());
 
 
 puppeteer.use(puppeteer_stealth());
+
 
 
 const Scrap = () =>{
@@ -45,21 +52,152 @@ await page.goto('http://preprod-casa-geored.geored.fr/desktop/index.php/document
 
 
 
+await clickpage(page,'.nestedThemeLnk ',0);
 
-await page.evaluate(async()=>{
-
-   var href = document.querySelectorAll('.nestedTopics div>a')
-
-   console.log(document.body)
-
-   })
-
-
+await setTimeout(async() => {
+    try{
+        await clickpage(page,'.nestedTopicLnk ',7);
+       setTimeout(async() => {
+          try{
+            await clickpage(page,'.nestedTopicLnk ',0);
+            setTimeout(async() => {
+              try{
+                await clickpage(page,'.nestedTopicLnk ',2);
+                setTimeout(async() => {
+                 await download(page);
+               },7000);
+              }catch{
+                setTimeout(async() => {
+                 await download(page);
+               },7000);
+              }
+           },7000);
+          }catch{
+            setTimeout(async() => {
+             await download(page);
+           },7000);
+          }
+       },10000);
+    }catch{
+        setTimeout(async() => {
+            await download(page);
+           },10000);
+    }
+},5000);
 
     })();
 }
 
 
+
+const clickpage=async(page,htmlclass,number)=>{
+  await page.evaluate((htmlclass,number)=>{
+      var href = document.querySelector('iframe').contentDocument.querySelectorAll(htmlclass)[number]
+
+      href.click();
+     
+
+  },htmlclass,number);
+}
+
+
+const download=async(page)=>{
+    var date = new Date();
+    
+          var result = await page.evaluate(()=>{
+            return  document.querySelector('iframe').contentDocument.querySelector("body").innerText;
+          })
+
+          
+          var name = await page.evaluate(()=>{
+            return  document.querySelector('iframe').contentDocument.querySelector("h1").textContent.trim().replaceAll(' ','-').replaceAll('é','e').replaceAll('?','');
+          })
+
+          var folder = name;
+
+        console.log(folder);
+          var rand = date.getTime().toString();
+
+       await fs.mkdirSync("./files/"+folder, {
+            recursive: true
+        });
+
+        await page.screenshot({path: "./files/"+folder+'/'+folder+"1.png"});
+
+        // await page.waitFor(5000);
+
+        // await page.evaluate(async() => {
+        //     await new Promise((resolve, reject) => {
+        //         var totalHeight = 0;
+        //         var distance = 100;
+        //         var timer = setInterval(() => {
+        //             var scrollHeight = document.body.scrollHeight;
+        //             window.scrollBy(0, distance);
+        //             totalHeight += distance;
+    
+        //             if (3000 >= scrollHeight) {
+        //                 clearInterval(timer);
+        //                 resolve();
+        //             }
+        //         }, 100);
+        //     });
+        // });
+
+        // await page.screenshot({path: "./files/"+folder+'/'+folder+"2.png"});
+
+        // Download Images 
+
+        var images  = await page.evaluate(()=>{
+            var data = [];
+            var img =  document.querySelector('iframe').contentDocument.querySelectorAll("img");
+            for(let i=0;i<img.length;i++){
+                data.push(img[i].src)
+            }
+
+            return data;
+        })
+
+        console.log(images);
+
+        for (let img=0;img<images.length;img++){
+
+          await setTimeout(async() => {
+            //    var base = "http://preprod-casa-geored.geored.fr";
+            // var url = new URL(images[img],base)
+            const encodedURI = encodeURI(images[img]);
+            const response =await fetch(encodedURI,{headers: {"Cookie" : "CAKEPHP=t2he21bhb4nl54g8jq351d8n30"}})
+            const buffer = await response.buffer();
+
+            var extenstion = ".png";
+            if(images[img].indexOf('.jpg')!=-1) extenstion = ".jpg"
+            if(images[img].indexOf('.jpeg')!=-1) extenstion = ".jpeg"
+
+             await fs.writeFileSync("./files/"+folder+"/"+date.getTime()+parseInt(Math.random()*22)+"-num-"+img+extenstion,buffer,(err,data)=>{
+              if(err) console.log(err)
+              else{
+                console.log('success',data)
+              }
+             })
+           },5000*img);
+        }
+
+    
+          fs.writeFile("./files/"+folder+"/"+date.getTime()+'.docx', result, err => {
+            if (err) {
+              console.error(err)
+              return;
+            }});
+}
+
+
+var downloadImg = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){
+      console.log('content-type:', res.headers['content-type']);
+      console.log('content-length:', res.headers['content-length']);
+  
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+  };
 
 
 module.exports =  {Scrap};
